eee4b3fc1ff70926bc2e529d58af6c29cc153051
haniehemat <hanihematasa@gmail.com>
2025-11-19 01:38:59 +0330

    blue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Mainpage } from './mainpage/mainpage';
 import { Aboutme } from './aboutme/aboutme';
 import { Education } from './education/education';
-import { Childhood } from './childhood/childhood';
 import { Skills } from './skills/skills';
 import { NotFoundpage } from './not-foundpage/not-foundpage';
 import { Informationpage } from './informationpage/informationpage';
@@ -10,7 +9,6 @@ import { Informationpage } from './informationpage/informationpage';
 export const routes: Routes = [
     { path: 'main', component: Mainpage },
     { path: 'about', component: Aboutme },
-    { path: 'childhood', component: Childhood },
     { path: 'education', component: Education },
     { path: 'skills', component: Skills },
     { path: 'NotFoundpage', component: NotFoundpage },
